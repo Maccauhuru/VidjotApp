5e8c1bd76abf692f9d0be409cd6b1741826031cf
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-03-23 20:20:04 -0500

    app.js routers

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const  express       = require('express'),
        app           = express(),
-       port          = 5000,
+       port          = 5000 || process.env.PORT,
        ip            = process.env.IP;
+
+       //Define Index Route
+
+       app.get('/',(req,res)=>{
+         res.send("Home Page Created!");
+       });
+
+
+    // Define About Route
+    app.get('/about',(req,res)=>{
+      res.send("About Page Created!");
+    });
 
 app.listen(port,()=> console.log(`Listening to port ${port}`));
