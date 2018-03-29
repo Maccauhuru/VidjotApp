22374a3fec280d95319ea84d293d26c15169420e
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-03-29 18:00:39 -0500

    add mongoose ,created DB & Idea Model

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ const express = require('express'),
   app = express(),
   port = 5000 || process.env.PORT,
   ip = process.env.IP,
-  exphbs = require('express-handlebars');
+  exphbs = require('express-handlebars'),
+  mongoose = require('mongoose');
+
+  mongoose.connect('mongodb://localhost/vidjot_db')
+  .then(()=>console.log(`MongoDB connected!`))
+  .catch((err)=>console.log(err));
+
+  //Load the Idea Model
+  require ('./models/Idea');
+  const Idea = mongoose.model('ideas');
 
 //Handlebars Middleware
 app.engine('handlebars', exphbs({
