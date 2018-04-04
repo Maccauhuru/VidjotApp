b33cb9ebad0d0a7345124b5ba00d7ee0343d3a80
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-04-04 17:42:29 -0500

    New routes ides & addIdeas

--- app.js
+++ app.js
@@ -3,7 +3,12 @@ const express = require('express'),
   port = 5000 || process.env.PORT,
   ip = process.env.IP,
   exphbs = require('express-handlebars'),
-  mongoose = require('mongoose');
+  mongoose = require('mongoose'),
+  bodyParser = require("body-parser");
+
+  //Body Parser Middleware
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
 
   mongoose.connect('mongodb://localhost/vidjot_db')
   .then(()=>console.log(`MongoDB connected!`))
@@ -29,6 +34,36 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   const subtitle = "This is an App for jotting down your next Youtube video ideas easily!";
   res.render('about',{subtitle:subtitle});
+});
+
+//Define Ideas Route
+app.get('/ideas',(req,res)=>{
+  res.send('Ideas Page');
+});
+
+//Process Ideas Form
+app.post('/ideas',(req,res)=>{
+let errors = [];
+if(!req.body.title){
+  errors.push({text : 'Please Add A Title'});
+}
+if(!req.body.details){
+  errors.push({text: 'Please Add Some Details'});
+}
+if(errors.length > 0){
+  res.render('ideas/add',{
+    errors:errors,
+    title:req.body.title,
+    details:req.body.details
+  });
+} else {
+  res.send("Sucessfully Posted!");
+}
+});
+
+//Define Add Ideas Route
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
 });
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
