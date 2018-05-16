3bcb01585fbac097081c41bd2619be6db0469e6e
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-05-15 20:09:01 -0500

    user access control method added

--- models/Idea.js
+++ models/Idea.js
@@ -12,10 +12,15 @@ details : {
   type:String,
   required: true
 },
+user: {
+type:String,
+required : true
+},
 date : {
   type:Date,
   default : Date.now()
 }
+
   });
 
 mongoose.model('ideas',IdeaSchema);
