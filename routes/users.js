b833c5b84a5f0ec41fbd55b7f66f48eff82b5b84
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-05-16 09:55:09 -0500

    Login form post redirects fixed

--- routes/users.js
+++ routes/users.js
@@ -65,8 +65,8 @@ router.post("/register", (req, res) => {
                 newUser
                   .save()
                   .then(user => {
-                    req.flash("succes_msg", "You are now registered and can log in");
-                    res.redirect("/users/register");
+                    req.flash("success_msg", "You are now registered and can log in");
+                    res.redirect("/users/login");
                   })
                   .catch(err => {
                     console.log(err);
