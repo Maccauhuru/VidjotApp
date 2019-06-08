495c4ba9e77a97bbaac22ad3a5f0e8e62d09922a
simba <simba3310@gmail.com>
2019-06-07 22:17:22 -0500

    config file

---
+++ config/database.js
@@ -0,0 +1,11 @@
+
+if (process.env.NODE_ENV === 'production0') {
+    module.exports = {
+        mongoURI: 'mongodb://admin:admin123@ds261626.mlab.com:61626/vidjot-prod'
+    }
+}
+else {
+    module.exports = {
+        mongoURI: 'mongodb://localhost/vidjot_DB'
+    }
+}
